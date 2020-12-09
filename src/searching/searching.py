9d8e6d7ef95e7c85c0473d59c5a85cee1e8f2ef3
Josh Glantz-Hucks <jahteo@gmail.com>
2020-12-08 22:27:52 -0800

    solved seraching.py & stretch

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -8,6 +8,27 @@ def binary_search(arr, target, start, end):
     #elif taget == endpoint, return index
 
     # return target index
+    # Check base case
+    if end >= start:
+        # print(arr)
+        mid = (end + start) // 2
+
+        # If element is present at the middle itself
+        if arr[mid] == target:
+            return mid
+
+        # If element is smaller than mid, then it can only
+        # be present in left subarray
+        elif arr[mid] > target:
+            return binary_search(arr, target, start, mid - 1)
+
+        # Else the element can only be present in right subarray
+        else:
+            return binary_search(arr, target, mid + 1, end)
+
+    else:
+        # Element is not present in the array
+        return -1
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -18,6 +39,13 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-    pass
+    print("SORTED(ARR)", sorted(arr))
+    if arr[0] < arr[-1]:
+        return binary_search(arr, target, 0, len(arr)-1)
+    else:
+        reverseIndex = binary_search(sorted(arr), target, 0, len(arr)-1)
+        if reverseIndex == -1:
+            return -1
+        return len(arr) - reverseIndex - 1
 # testin testin juan
 
